9fd6c262422e8a7ae918d40bdab3b19744055591
tanish <tanish.afre@gmail.com>
2023-12-11 16:26:21 +0000

    imports

--- smart-Tech-CA1.py
+++ smart-Tech-CA1.py
@@ -1,4 +1,5 @@
-from tensorflow.keras import datasets, layers, models
+import tensorflow as tf
+from tensorflow.karas import datasets, layers, models
 import matplotlib.pyplot as plt
 import numpy as np
 
