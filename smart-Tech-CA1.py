513f61e26da9afd8286d8cf4a6335264ece96967
tanish <tanish.afre@gmail.com>
2023-12-11 16:49:19 +0000

    adding git Ignore

--- smart-Tech-CA1.py
+++ smart-Tech-CA1.py
@@ -2,4 +2,5 @@ import tensorflow as tf
 from tensorflow.karas import datasets, layers, models
 import matplotlib.pyplot as plt
 import numpy as np
+(X_train, y_train), (X_test, y_test) = datasets.cifar10.load_data()
 
