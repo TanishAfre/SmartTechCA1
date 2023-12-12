cf91330a3407622d9908a93408b3278c97eaa842
Kevin Daly <92157588+D00237535@users.noreply.github.com>
2023-12-12 19:18:24 +0000

    Added the load image function and the __main__

--- smart-Tech-CA1.py
+++ smart-Tech-CA1.py
@@ -1,6 +1,19 @@
-import tensorflow as tf
-from tensorflow.karas import datasets, layers, models
-import matplotlib.pyplot as plt
-import numpy as np
-(X_train, y_train), (X_test, y_test) = datasets.cifar10.load_data()
+# Made with the use of Github Co-Pilot
 
+import numpy as np
+from tensorflow.keras.datasets import cifar10, cifar100
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import cv2
+
+def load_cifar_datasets():
+    # Load CIFAR-10 and CIFAR-100 datasets
+    (cifar10_train_images, cifar10_train_labels), _ = cifar10.load_data()
+    (cifar100_train_images, cifar100_train_labels), _ = cifar100.load_data()
+    return cifar10_train_images, cifar10_train_labels, cifar100_train_images, cifar100_train_labels
+
+
+
+if __name__ == "__main__":
+    cifar10_train_images, cifar10_train_labels, cifar100_train_images, cifar100_train_labels = load_cifar_datasets()
+    
