ee702ab2c31c87d17eb48c224b7d7b47a8868d84
Kevin Daly <92157588+D00237535@users.noreply.github.com>
2023-12-15 16:27:03 +0000

    Spilt the data_Visualization code into its own file

---
+++ data_visualization.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def plot_sample_images(images):
+    plt.figure(figsize=(10, 10))
+    for i in range(min(len(images), 24)):
+        plt.subplot(5, 5, i + 1)
+        # Check if the image is grayscale or color
+        if len(images[i].shape) == 3 and images[i].shape[2] == 3:
+            plt.imshow(images[i])
+        else:
+            plt.imshow(images[i].reshape((32, 32)), cmap='gray')
+        plt.axis('off')
+    plt.show()
+
+def display_training_images(images):
+    plt.figure(figsize=(10, 10))
+    for i in range(min(len(images), 24)):
+        plt.subplot(5, 5, i + 1)
+        # Check if the image is grayscale or color
+        if len(images[i].shape) == 3 and images[i].shape[2] == 3:
+            plt.imshow(images[i])
+        else:
+            plt.imshow(images[i].reshape((32, 32)), cmap='gray')
+        plt.axis('off')
+    plt.show()
