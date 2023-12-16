0d113063e667b14f34f9ed80aa490fd0367ca5b0
Kevin Daly <92157588+D00237535@users.noreply.github.com>
2023-12-16 23:01:07 +0000

    Testing Image augmentation

--- data_visualization.py
+++ data_visualization.py
@@ -25,3 +25,17 @@ def display_training_images(images):
             plt.imshow(images[i].reshape((32, 32)), cmap='gray')
         plt.axis('off')
     plt.show()
+
+    # display 10 random augmented images
+def plot_sample_augmented_images(images):
+    plt.figure(figsize=(10, 10))
+    for i in range(min(len(images), 10)):
+        plt.subplot(5, 5, i + 1)
+        # Check if the image is grayscale or color
+        if len(images[i].shape) == 3 and images[i].shape[2] == 3:
+            plt.imshow(images[i])
+        else:
+            plt.imshow(images[i].reshape((32, 32)), cmap='gray')
+        plt.axis('off')
+    plt.show()
+    
